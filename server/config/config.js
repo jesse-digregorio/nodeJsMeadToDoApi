4ca7d318360ff8518297de840cb30de3db8231d6
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-10-01 21:33:31 -0700

    added POST /user route

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,6 @@ console.log('env *******', env);
 
 switch(env) {
     case 'development':
-        code block
         process.env.PORT = 3000;
         process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
         break;
--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,45 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+// {
+//     email: 'jesse@example.com',
+//     password: 'myPass123',
+//     tokens: [{
+//         access: 'auth',
+//         token: ''
+//     }]
+//
+// }
 
 var User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         trim: true,
-        minLength: 1
-    }
+        minLength: 1,
+        unique: true,
+        validate: {
+            validator: (value) => {
+                validator.isEmail(value);
+            },
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minLength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 module.exports = {User}
