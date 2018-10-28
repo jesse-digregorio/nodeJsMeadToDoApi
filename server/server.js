4205367105302502598bbb5af151ea8994f94410
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-10-27 17:52:55 -0700

    added POST /users/login route

--- server/server.js
+++ server/server.js
@@ -129,6 +129,22 @@ app.post('/users', (req, res) => {
     })
 });
 
+// POST /users/login
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    console.log('/users/login - BODY: ', body);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+});
+
 
 
 
