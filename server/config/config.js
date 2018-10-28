61c9ca301f171a6ba30e7c2a7cfee8f2a0005bfb
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-10-28 15:07:13 -0700

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,25 @@
 var env = process.env.NODE_ENV || 'development';
 console.log('env *******', env);
 
-switch(env) {
-    case 'development':
-        process.env.PORT = 3000;
-        process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
-        break;
-    case 'test':
-        process.env.PORT = 3000;
-        process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
-        break;
-    default:
-        // meh.
+if (env === 'developemnt' || env === 'test') {
+    var config = require('./config.json');
+    console.log(config);
+
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
+}
+
+
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
